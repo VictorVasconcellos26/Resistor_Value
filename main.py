26ecb6cad86859346d6b37c50f2a43307b3deba2
Victor Vasconcellos <mr.victor2319@gmail.com>
2021-11-12 10:50:04 -0300

    Dict-Codigo-de-cores

---
+++ main.py
@@ -0,0 +1,11 @@
+resistor_cores = {'Preto': '0',
+                  'Marrom': '1',
+                  'Vermelho': '2',
+                  'Laranja': '3', 'Amarelo': '4',
+                  'Verde': '5',
+                  'Azul': '6',
+                  'Violeta': '7',
+                  'Cinza': '8',
+                  'Branco': '9'
+                  }
+print(resistor_cores)
