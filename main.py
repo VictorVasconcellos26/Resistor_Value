eac6407ee18b40938090e34b473693cd64069f28
Victor Vasconcellos <mr.victor2319@gmail.com>
2021-11-13 05:36:48 -0300

    Trocando o nome das variaveis de PT-BR para Inglês

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-resistor_cores = {'0': '1',
-                  '1': '10',
-                  '2': '100',
-                  '3': '1000',
-                  '4': '10000',
-                  '5': '100000',
-                  '6': '1000000',
-                  '7': '10000000',
-                  '8': '100000000',
-                  '9': '1000000000'
-                  }
+resistor_colors = {'0': '1',
+                   '1': '10',
+                   '2': '100',
+                   '3': '1000',
+                   '4': '10000',
+                   '5': '100000',
+                   '6': '1000000',
+                   '7': '10000000',
+                   '8': '100000000',
+                   '9': '1000000000'
+                   }
 
-numero = input("Digite o código solicitado: ")
+number = input("Digite o código solicitado: ")
 
 
-def codigo():
-    multiple = numero[2]
-    valor_resistor = int(numero[0] + numero[1])
-    valor_resistor = valor_resistor * int(resistor_cores.setdefault(multiple))
+def code():
+    multiplier = number[2]
+    valor_resistor = int(number[0] + number[1])
+    valor_resistor = valor_resistor * int(resistor_colors.setdefault(multiplier))
     valor_resistor2 = f"{valor_resistor:_.1f}"
-    valor_resistor2 = valor_resistor2.replace('.',',').replace('_','.')
-    print(f"O valor do resistor de código ({numero}) é de {valor_resistor2} ohms!")
+    valor_resistor2 = valor_resistor2.replace('.', ',').replace('_', '.')
+    print(f"O valor do resistor de código ({number}) é de {valor_resistor2} ohms!")
     return
 
-codigo()
+
+code()
+
