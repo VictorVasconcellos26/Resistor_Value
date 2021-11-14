0ffa5612e46d9058c8f39c1de807d723e009a465
Victor Vasconcellos <mr.victor2319@gmail.com>
2021-11-14 14:37:32 -0300

    Função para determinar, a parti de um dicionario pré-programado, qual cor o código escolhido representa.

--- main.py
+++ main.py
@@ -36,7 +36,20 @@ def code():
     value_resistor_default = value_resistor_default * int(resistor_colors.setdefault(multiplier))
     value_resistor_index = f"{value_resistor_default:_.1f}"
     value_resistor_index = value_resistor_index.replace('.', ',').replace('_', '.')
+    colors_reference()
+    print("")
     print(f"O valor do resistor de código ({number}) é de {value_resistor_index} ohms!")
+    return
+
+
+def colors_reference():
+    print("""============================
+As cores selecionadas foram:
+============================
+    """)
+    for colors in number:
+        reference_number = resistor_patters.setdefault(colors)
+        print(colors,":",reference_number)
     return
 
 
